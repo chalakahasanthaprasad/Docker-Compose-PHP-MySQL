362a42e0980ec4f04f422599abff815b081e8eb1
chalakahasanthaprasad <amchalakahasantha@gmail.com>
2024-06-28 22:41:11 +0530

    CDN -bootstrap,metisMenu,sb-admin,jquery,add ide bar dashboard

---
+++ includes/sidebar.php
@@ -0,0 +1,31 @@
+<!-- Sidebar -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <!-- <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">Student Management</a>
+    </div> -->
+    <!-- /.navbar-header -->
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+                <li>
+                    <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-user fa-fw"></i> Students<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="add_student.php">Add Student</a>
+                        </li>
+                        <li>
+                            <a href="view_students.php">View Students</a>
+                        </li>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+    <!-- /.navbar-static-side -->
+</nav>
+<!-- /#sidebar-wrapper -->
--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -1,13 +1,43 @@
-<?php
+<?php include '../../includes/headerN.php'; ?>
 
-session_start();
+<div id="wrapper">
+    <?php include '../../includes/sidebar.php'; ?>
 
-// Check if user is not logged in, redirect to login page
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header('Location: login.php');
-    exit;
-}
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Student Management Dashboard</h1>
+                </div>
+            </div>
+            <!-- Your content here -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Student Information
+                        </div>
+                        <div class="panel-body">
+                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>Student ID</th>
+                                        <th>Name</th>
+                                        <th>Class</th>
+                                        <th>Age</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- Add PHP code here to fetch and display students from the database -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>    
 
-echo "Hello World Your Login to Server";
-
-?>
+<?php include '../../includes/footer.php'; ?>
