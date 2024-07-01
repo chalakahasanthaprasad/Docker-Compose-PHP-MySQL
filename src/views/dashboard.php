cb6b9ec109b1a76013cdfaff8f9241f92a90a488
chalakahasanthaprasad <amchalakahasantha@gmail.com>
2024-07-01 22:39:45 +0530

    session uncomment

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -3,74 +3,74 @@ session_start();
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("location: login.php");
     exit;
-    // } else {
-    //     $now = time(); // Checking the time now when home page starts.
+} else {
+    $now = time(); // 30 min -  Checking the time now when home page starts.
 
-    //     if ($now > $_SESSION['expire']) {
-    //         session_destroy();
-    //         echo "Your session has expired! <a href='http://localhost/src/views/login.php'>Login here</a>";
-    //     } else { //Starting this else one [else1]
+    if ($now > $_SESSION['expire']) {
+        session_destroy();
+        echo "Your session has expired! <a href='http://localhost/src/views/login.php'>Login here</a>";
+    } else {
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+        <!DOCTYPE html>
+        <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Student Management System</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.9/metisMenu.min.css" rel="stylesheet">
-        <link href="../../assets/css/sb-admin-2.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    </head>
+        <head>
+            <meta charset="UTF-8">
+            <title>Student Management System</title>
+            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.9/metisMenu.min.css" rel="stylesheet">
+            <link href="../../assets/css/sb-admin-2.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        </head>
 
-    <body>
-        <div id="wrapper">
-            <?php include('../../includes/sidebar.php') ?>;
+        <body>
+            <div id="wrapper">
+                <?php include('../../includes/sidebar.php') ?>;
 
-            <div id="page-wrapper">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h4 class="page-header"> <?php echo strtoupper("welcome" . " " . htmlentities($_SESSION['username'])); ?></h4>
+                <div id="page-wrapper">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h4 class="page-header"> <?php echo strtoupper("welcome" . " " . htmlentities($_SESSION['username'])); ?></h4>
+                            </div>
+                            <!-- /.col-lg-12 -->
                         </div>
-                        <!-- /.col-lg-12 -->
-                    </div>
-                    <!-- Your content here -->
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    Student Information
-                                </div>
-                                <div class="panel-body">
-                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                        <thead>
-                                            <tr>
-                                                <th>Student ID</th>
-                                                <th>Name</th>
-                                                <th>Class</th>
-                                                <th>Age</th>
-                                                <th>Actions</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <!-- Add PHP code here to fetch and display students from the database -->
-                                        </tbody>
-                                    </table>
+                        <!-- Your content here -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        Student Information
+                                    </div>
+                                    <div class="panel-body">
+                                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                            <thead>
+                                                <tr>
+                                                    <th>Student ID</th>
+                                                    <th>Name</th>
+                                                    <th>Class</th>
+                                                    <th>Age</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <!-- Add PHP code here to fetch and display students from the database -->
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <?php include '../../includes/datetime.php'; ?>
+            <?php include '../../includes/datetime.php'; ?>
 
-        <!-- <?php
-            }
-            // }
-                ?> -->
+    <?php
+    }
+}
+    ?>
 
 
-        <?php include '../../includes/footer.php'; ?>
+    <?php include '../../includes/footer.php'; ?>
