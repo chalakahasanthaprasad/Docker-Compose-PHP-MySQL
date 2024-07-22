d6a6f2d928ab425c2abccfd57868127487e75430
chalakahasanthaprasad <amchalakahasantha@gmail.com>
2024-07-22 23:53:51 +0530

    change subject tbl structure and add new course_subjects

--- db/nibm.sql
+++ db/nibm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Jul 15, 2024 at 06:05 PM
+-- Generation Time: Jul 22, 2024 at 06:22 PM
 -- Server version: 8.0.37
 -- PHP Version: 8.2.8
 
@@ -20,6 +20,105 @@ SET time_zone = "+00:00";
 --
 -- Database: `nibm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_subjects`
+--
+
+CREATE TABLE `course_subjects` (
+  `course_id` int NOT NULL,
+  `subject_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course_subjects`
+--
+
+INSERT INTO `course_subjects` (`course_id`, `subject_id`) VALUES
+(1, 1),
+(10, 1),
+(1, 2),
+(10, 2),
+(1, 3),
+(10, 3),
+(1, 4),
+(10, 4),
+(1, 5),
+(10, 5),
+(2, 6),
+(9, 6),
+(13, 6),
+(14, 6),
+(2, 7),
+(9, 7),
+(13, 7),
+(14, 7),
+(2, 8),
+(9, 8),
+(13, 8),
+(14, 8),
+(2, 9),
+(9, 9),
+(13, 9),
+(14, 9),
+(3, 10),
+(7, 10),
+(8, 10),
+(3, 11),
+(7, 11),
+(8, 11),
+(3, 12),
+(7, 12),
+(8, 12),
+(3, 13),
+(7, 13),
+(8, 13),
+(4, 14),
+(4, 15),
+(4, 16),
+(4, 17),
+(5, 18),
+(5, 19),
+(5, 20),
+(5, 21),
+(6, 22),
+(6, 23),
+(6, 24),
+(6, 25),
+(11, 26),
+(12, 26),
+(11, 27),
+(12, 27),
+(11, 28),
+(12, 28),
+(11, 29),
+(12, 29),
+(15, 30),
+(16, 30),
+(15, 31),
+(16, 31),
+(15, 32),
+(16, 32),
+(15, 33),
+(16, 33),
+(17, 34),
+(18, 34),
+(17, 35),
+(18, 35),
+(17, 36),
+(18, 36),
+(17, 37),
+(18, 37),
+(19, 38),
+(20, 38),
+(19, 39),
+(20, 39),
+(19, 40),
+(20, 40),
+(19, 41),
+(20, 41);
 
 -- --------------------------------------------------------
 
@@ -83,7 +182,7 @@ CREATE TABLE `tbl_course` (
 --
 
 INSERT INTO `tbl_course` (`cid`, `code`, `cfull`, `created_date`, `update_date`) VALUES
-(1, 'MCA', 'Master of Computer Applications', '2024-03-01', NULL),
+(1, 'MCA', 'Master of Computer Applications', '2024-03-01', '19-07-2024'),
 (2, 'B.E.', 'Bachelor of Engineering', '2024-03-02', NULL),
 (3, 'B.Sc.', 'Bachelor of Science', '2024-03-03', NULL),
 (4, 'B.Com.', 'Bachelor of Commerce', '2024-03-04', NULL),
@@ -102,7 +201,8 @@ INSERT INTO `tbl_course` (`cid`, `code`, `cfull`, `created_date`, `update_date`)
 (17, 'B.Ed.', 'Bachelor of Education', '2024-03-17', NULL),
 (18, 'M.Ed.', 'Master of Education', '2024-03-18', NULL),
 (19, 'B.Pharm.', 'Bachelor of Pharmacy', '2024-03-19', NULL),
-(20, 'M.Pharm.', 'Master of Pharmacy', '2024-03-20', NULL);
+(20, 'M.Pharm.', 'Master of Pharmacy', '2024-03-20', NULL),
+(21, 'Test', 'TestFull', '15-07-2024', '17-07-2024');
 
 -- --------------------------------------------------------
 
@@ -150,7 +250,7 @@ CREATE TABLE `tbl_student` (
 --
 
 INSERT INTO `tbl_student` (`std_id`, `std_name`, `course_code`, `gender`, `address`, `birthofdate`, `mobile_number`, `parent_number`, `registered_date`, `city_id`) VALUES
-(1, 'Nimal Perera', 'MCA', 'Male', '123 Galle Road, Colombo', '2000-01-01', '0712345678', '0778765432', '2024-03-01', 5),
+(1, 'Nimal', 'MCA', 'Male', '123 Galle Road, Colombo', '2000-01-01', '0712345678', '0778765432', '2024-03-01', 5),
 (2, 'Kamal Fernando', 'B.E.', 'Male', '456 Kandy Road, Kandy', '1999-02-15', '0712345679', '0778765433', '2024-03-02', 11),
 (3, 'Sunil Silva', 'B.Sc.', 'Male', '789 Matara Road, Galle', '2001-03-20', '0712345680', '0778765434', '2024-03-03', 6),
 (4, 'Amara Jayawardena', 'B.Com.', 'Female', '101 Kurunegala Road, Kurunegala', '2002-04-25', '0712345681', '0778765435', '2024-03-04', 14),
@@ -201,39 +301,139 @@ INSERT INTO `tbl_student` (`std_id`, `std_name`, `course_code`, `gender`, `addre
 (49, 'Menaka Jayathilaka', 'B.Com.', 'Female', '4646 Galle Road, Colombo', '2003-01-10', '0712345726', '0778765480', '2024-04-18', 5),
 (50, 'Ravindu Jayasuriya', 'MBA', 'Male', '4747 Kandy Road, Kandy', '2004-02-15', '0712345727', '0778765481', '2024-04-19', 11),
 (51, 'testFemale', '', 'Female', 'col 7', '2002-07-11', '0715116262', '0725445545', '2024-07-11', 5),
-(52, 'testMale', 'MCA', 'Male', 'aaa', '2000-07-11', '1111111111', '2222222222', '2024-07-11', 7);
+(53, 'test', 'B.E.', 'Female', 'AAA', '2004-07-20', '0111111111', '0222222222', '2024-07-20', 16);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_subject`
+-- Table structure for table `tbl_subjects`
 --
 
-CREATE TABLE `tbl_subject` (
-  `subid` int NOT NULL,
-  `code` varchar(50) DEFAULT NULL,
-  `cfull` varchar(250) DEFAULT NULL,
-  `sub1` varchar(250) DEFAULT NULL,
-  `sub2` varchar(250) DEFAULT NULL,
-  `sub3` varchar(250) DEFAULT NULL,
-  `sub4` varchar(255) DEFAULT NULL,
+CREATE TABLE `tbl_subjects` (
+  `subject_id` int NOT NULL,
+  `subject_code` varchar(50) NOT NULL,
+  `subject_name` varchar(250) NOT NULL,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_date` varchar(200) DEFAULT NULL
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_subject`
+-- Dumping data for table `tbl_subjects`
 --
 
-INSERT INTO `tbl_subject` (`subid`, `code`, `cfull`, `sub1`, `sub2`, `sub3`, `sub4`, `created_date`, `update_date`) VALUES
-(1, '2', 'Bachelor of Engineering', 'Computer Science', 'Electrical Engineering', 'Mechanical Engineering', 'Mathematics', '2024-03-01 09:00:00', NULL),
-(2, '3', 'Bachelor of Science', 'Biology', 'Chemistry', 'Physics', 'Environmental Science', '2024-03-02 10:30:00', NULL),
-(3, '4', 'Bachelor of Commerce', 'Accounting', 'Business Management', 'Marketing', 'Economics', '2024-03-03 11:45:00', NULL),
-(4, '5', 'Master of Business Administration', 'Finance', 'Human Resource Management', 'Strategic Management', 'Operations Management', '2024-03-04 12:15:00', NULL);
+INSERT INTO `tbl_subjects` (`subject_id`, `subject_code`, `subject_name`, `created_date`, `update_date`) VALUES
+(1, 'CS101', 'Computer Science Basics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(2, 'CS102', 'Data Structures', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(3, 'CS103', 'Algorithms', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(4, 'CS104', 'Operating Systems', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(5, 'CS105', 'Database Management Systems', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(6, 'ENG101', 'Engineering Mathematics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(7, 'ENG102', 'Mechanics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(8, 'ENG103', 'Thermodynamics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(9, 'ENG104', 'Fluid Mechanics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(10, 'SCI101', 'Physics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(11, 'SCI102', 'Chemistry', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(12, 'SCI103', 'Biology', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(13, 'SCI104', 'Mathematics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(14, 'COM101', 'Financial Accounting', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(15, 'COM102', 'Business Law', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(16, 'COM103', 'Economics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(17, 'COM104', 'Marketing', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(18, 'MBA101', 'Management Principles', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(19, 'MBA102', 'Organizational Behavior', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(20, 'MBA103', 'Financial Management', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(21, 'MBA104', 'Human Resource Management', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(22, 'BBA101', 'Introduction to Business', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(23, 'BBA102', 'Business Communication', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(24, 'BBA103', 'Business Statistics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(25, 'BBA104', 'Business Ethics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(26, 'LAW101', 'Constitutional Law', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(27, 'LAW102', 'Criminal Law', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(28, 'LAW103', 'Civil Law', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(29, 'LAW104', 'Corporate Law', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(30, 'ARCH101', 'Architectural Design', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(31, 'ARCH102', 'Building Materials', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(32, 'ARCH103', 'Structural Systems', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(33, 'ARCH104', 'Urban Planning', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(34, 'EDU101', 'Educational Psychology', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(35, 'EDU102', 'Curriculum Development', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(36, 'EDU103', 'Educational Technology', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(37, 'EDU104', 'Assessment Methods', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(38, 'PHARM101', 'Pharmacology', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(39, 'PHARM102', 'Medicinal Chemistry', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(40, 'PHARM103', 'Pharmaceutics', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(41, 'PHARM104', 'Pharmacy Practice', '2024-07-22 17:42:48', '2024-07-22 17:42:48'),
+(42, 'CS106', 'Software Engineering', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(43, 'CS107', 'Computer Networks', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(44, 'CS108', 'Artificial Intelligence', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(45, 'CS109', 'Machine Learning', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(46, 'CS110', 'Cyber Security', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(47, 'CS111', 'Web Development', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(48, 'ENG107', 'Materials Science', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(49, 'ENG108', 'Control Systems', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(50, 'ENG109', 'Signals and Systems', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(51, 'ENG110', 'Digital Electronics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(52, 'ENG111', 'Heat Transfer', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(53, 'ENG112', 'Manufacturing Processes', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(54, 'SCI107', 'Organic Chemistry', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(55, 'SCI108', 'Inorganic Chemistry', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(56, 'SCI109', 'Quantum Mechanics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(57, 'SCI110', 'Thermodynamics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(58, 'SCI111', 'Genetics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(59, 'SCI112', 'Ecology', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(60, 'COM107', 'Taxation', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(61, 'COM108', 'Auditing', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(62, 'COM109', 'Corporate Governance', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(63, 'COM110', 'Investment Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(64, 'COM111', 'Cost Accounting', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(65, 'COM112', 'International Business', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(66, 'MBA107', 'Entrepreneurship', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(67, 'MBA108', 'Operations Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(68, 'MBA109', 'Supply Chain Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(69, 'MBA110', 'Project Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(70, 'MBA111', 'Business Analytics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(71, 'MBA112', 'Leadership and Change Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(72, 'BBA107', 'International Business', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(73, 'BBA108', 'Financial Markets', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(74, 'BBA109', 'Entrepreneurship', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(75, 'BBA110', 'E-commerce', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(76, 'BBA111', 'Retail Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(77, 'BBA112', 'Quality Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(78, 'LAW107', 'Labor Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(79, 'LAW108', 'Environmental Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(80, 'LAW109', 'Human Rights Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(81, 'LAW110', 'Tax Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(82, 'LAW111', 'Family Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(83, 'LAW112', 'Property Law', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(84, 'ARCH107', 'Interior Design', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(85, 'ARCH108', 'Architectural History', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(86, 'ARCH109', 'Building Construction', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(87, 'ARCH110', 'Sustainable Design', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(88, 'ARCH111', 'Digital Architecture', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(89, 'ARCH112', 'Housing Design', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(90, 'EDU106', 'Classroom Management', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(91, 'EDU107', 'Special Education', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(92, 'EDU108', 'Educational Leadership', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(93, 'EDU109', 'Child Development', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(94, 'EDU110', 'Instructional Strategies', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(95, 'EDU111', 'Counseling in Education', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(96, 'PHARM105', 'Clinical Pharmacy', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(97, 'PHARM106', 'Pharmacokinetics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(98, 'PHARM107', 'Pharmaceutical Chemistry', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(99, 'PHARM108', 'Pharmacognosy', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(100, 'PHARM109', 'Biopharmaceutics', '2024-07-22 17:53:02', '2024-07-22 17:53:02'),
+(101, 'PHARM110', 'Pharmaceutical Analysis', '2024-07-22 17:53:02', '2024-07-22 17:53:02');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `course_subjects`
+--
+ALTER TABLE `course_subjects`
+  ADD PRIMARY KEY (`course_id`,`subject_id`),
+  ADD KEY `subject_id` (`subject_id`);
 
 --
 -- Indexes for table `tbl_city`
@@ -260,10 +460,10 @@ ALTER TABLE `tbl_student`
   ADD PRIMARY KEY (`std_id`);
 
 --
--- Indexes for table `tbl_subject`
+-- Indexes for table `tbl_subjects`
 --
-ALTER TABLE `tbl_subject`
-  ADD PRIMARY KEY (`subid`);
+ALTER TABLE `tbl_subjects`
+  ADD PRIMARY KEY (`subject_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -279,7 +479,7 @@ ALTER TABLE `tbl_city`
 -- AUTO_INCREMENT for table `tbl_course`
 --
 ALTER TABLE `tbl_course`
-  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `tbl_login`
@@ -291,13 +491,24 @@ ALTER TABLE `tbl_login`
 -- AUTO_INCREMENT for table `tbl_student`
 --
 ALTER TABLE `tbl_student`
-  MODIFY `std_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `std_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
--- AUTO_INCREMENT for table `tbl_subject`
+-- AUTO_INCREMENT for table `tbl_subjects`
 --
-ALTER TABLE `tbl_subject`
-  MODIFY `subid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `tbl_subjects`
+  MODIFY `subject_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `course_subjects`
+--
+ALTER TABLE `course_subjects`
+  ADD CONSTRAINT `course_subjects_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `tbl_course` (`cid`) ON DELETE CASCADE,
+  ADD CONSTRAINT `course_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `tbl_subjects` (`subject_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
