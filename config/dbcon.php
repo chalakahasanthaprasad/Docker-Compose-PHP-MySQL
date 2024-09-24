06e4ce02be5acc3a840165b2d7e90400442d6ece
chalakahasanthaprasad <amchalakahasantha@gmail.com>
2024-09-25 00:03:03 +0530

    Add ENV file Params

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,10 +1,10 @@
 <?php
 
-// Database connection 
-$host = "db"; # docker compose service name
-$username = "root";
-$password = "root";
-$database = "nibm";
+// Database connection parameters
+$host = getenv('MYSQL_HOST', true) ?: getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER', true) ?: getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD', true) ?: getenv('MYSQL_PASSWORD');
+$database = getenv('MYSQL_DATABASE', true) ?: getenv('MYSQL_DATABASE');
 
 // Create a database connection
 $connect = mysqli_connect($host, $username, $password, $database);
