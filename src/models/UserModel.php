6eb6842cbf04f91e817184b14482b779a4bffc8b
chalakahasanthaprasad <amchalakahasantha@gmail.com>
2024-09-30 23:58:54 +0530

    Update User Model (prepared statement + try Catch)

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-// models/User.php
 class UserModel
 {
     private $conn;
@@ -7,25 +6,37 @@ class UserModel
     public function __construct($db)
     {
         $this->conn = $db;
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     }
 
     public function getUserByUsername($username)
     {
-        $stmt = $this->conn->prepare("SELECT id,loginid, password FROM tbl_login WHERE loginid = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $stmt->store_result();
-        //initializing
-        $id = $username = $password = null;
+        try {
+            $stmt = $this->conn->prepare("SELECT id, loginid, password FROM tbl_login WHERE loginid = ?");
+            if (!$stmt) {
+                throw new Exception("Failed to prepare statement: " . $this->conn->error);
+            }
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $stmt->store_result();
+            $id = $loginid = $password = null;
 
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($id, $username, $password);
-            $stmt->fetch();
-            //echo "$id, $username, $password";
-            return ['id' => $id, 'username' => $username, 'password' => $password];
+            if ($stmt->num_rows > 0) {
+                $stmt->bind_result($id, $loginid, $password);
+                $stmt->fetch();
+                return ['id' => $id, 'username' => $loginid, 'password' => $password];
+            } else {
+                return null;
+            }
+
+        } catch (Exception $e) {
+            error_log("Error in getUserByUsername: " . $e->getMessage());
+            return false;
+        } finally {
+            if ($stmt) {
+                $stmt->close();
+            }
         }
-
-        return null;
     }
 }
 ?>
